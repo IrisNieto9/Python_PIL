a7995fb2c865945fbf9ef67f3888a53a6147f156
unknown <IrisNieto9>
2022-09-04 18:13:08 -0300

    Entradas y salidas de datos

---
+++ Clase_1/entrada_salida_datos.py
@@ -0,0 +1,26 @@
+# Salida de datos
+
+print("Hola Mundo...\n")
+print("Desde' \t visual")
+
+# Otras funciones del print
+# Salto de linea
+print('Esto es un salto \n de linea')
+
+# Tabulacion
+print('Con esto se \t tabula')
+
+# Concatenar
+print("Hola" + "Mundo")
+
+# Sep
+print('Hola', 'Mundo\n', sep='+')
+
+# End
+print("Hola Mundo...", end='\t')
+print("Desde' visual")
+
+
+# Entrada de datos
+
+input('Ingrese un mensaje: ')
