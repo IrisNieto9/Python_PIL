3d75fca46df92da6153dcdba49d979f9d7d961df
IrisNieto9 <iris.nieto.montoya@mi.unc.edu.ar>
2022-09-06 21:31:25 -0300

    Clase 2 sin terminar

---
+++ Clase_2/condicionales.py
@@ -0,0 +1,25 @@
+# Condicionales
+edad_juan = 16
+
+if edad_juan >= 16 or edad_juan >= 18:
+    print('Juan puede votar y es mayor de edad')
+else:
+    print('No se cumple alguna de las condiciones')
+
+# Elif
+
+a = 5
+
+if a == 3:
+    print('A es iguala 3')
+elif a == 4:
+    print('A es igual a 4')
+elif a == 5:
+    print('A es igual a 5')
+else:
+    print('A no es igual a nada')
+
+
+
+if a == 3:
+    print('A es iguala 3')
